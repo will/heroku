741e3a75fcde16a2cec3c0b4f8297bc52862e459
Will Leinweber <will@bitfission.com>
2012-09-21 14:01:36 -0500

    fix spec

--- spec/heroku/command/pg_spec.rb
+++ spec/heroku/command/pg_spec.rb
@@ -9,6 +9,7 @@ module Heroku::Command
       api.post_app "name" => "myapp"
       api.put_config_vars "myapp", {
         "DATABASE_URL" => "postgres://database_url",
+        "SHARED_DATABASE_URL" => "postgres://shared_database_url",
         "HEROKU_POSTGRESQL_IVORY_URL" => "postgres://database_url",
         "HEROKU_POSTGRESQL_RONIN_URL" => "postgres://ronin_database_url"
       }
